03ee51553ec6d3c2494b3722118066c67a582edc
isg209.x11.moi <isg209.x11.moi@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-03-25 10:02:03 +0000

    Mejora del diagrama de diseño para que se vea mejor y le he añadido las relaciones que le faltaban.

--- taxiFree/src/modelado/Trayecto.java
+++ taxiFree/src/modelado/Trayecto.java
@@ -3,13 +3,4 @@ package modelado;
 
 public class Trayecto implements IViajeStrategy {
 
-	@Override
-	public Viaje getViaje() {
-		return null;
-	}
-
-	@Override
-	public void setViaje(Viaje viaje) {
-	}
-
 }
