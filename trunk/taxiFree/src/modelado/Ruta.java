9b0281eb0e8b9ce49221a57d2fdd2cf47e75b11a
isg209.x11.moi@gmail.com <isg209.x11.moi@gmail.com@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-18 17:24:12 +0000

    Modificacion de algunas clases y construccion del modelado para mi parte.

--- trunk/taxiFree/src/modelado/Ruta.java
+++ trunk/taxiFree/src/modelado/Ruta.java
@@ -1,10 +1,31 @@
 package modelado;
 
+import java.util.Date;
 
-public class Ruta implements IViajeStrategy {
+
+public class Ruta {
 	
 	public String origen;
 	public String destino;
 	public Integer desplazamiento;
+	public Date fecha;
 	
+	public String getOrigen() {
+		return origen;
+	}
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public Integer getDesplazamiento() {
+		return desplazamiento;
+	}
+	public void setDesplazamiento(Integer desplazamiento) {
+		this.desplazamiento = desplazamiento;
+	}
 }
