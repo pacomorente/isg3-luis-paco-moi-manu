0a8828a3f2dc252ed1f9b3bcca8bbf09ced3c8c6
pacomorente <pacomorente@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-24 23:53:26 +0000

    Se añade el método delete en JDBCViaje para complementar al realizado en JDBCTrayecto

--- trunk/taxiFree/src/data/JDBCViajeDAO.java
+++ trunk/taxiFree/src/data/JDBCViajeDAO.java
@@ -1,14 +1,35 @@
 package data;
 
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import domain.Viaje;
 
 public class JDBCViajeDAO implements IViajeDAO {
 
 	@Override
-	public void deleteViaje(String numViaje) {
-		// TODO Auto-generated method stub
+	public void deleteViaje(Connection conn, String ViajeOID) {
+        String sql = "DELETE FROM Viaje WHERE (OID = ?) ";
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, ViajeOID);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Message: " + e.getMessage());
+            System.err.println("SQLState: " + e.getSQLState());
+            System.err.println("ErrorCode: " + e.getErrorCode());
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+            }
+        }
+	    
 
 	}
 
