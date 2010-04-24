ee3afe02d022f7bd110f67f874557eab8637105f
pacomorente <pacomorente@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-24 23:55:47 +0000

    Se añade la posible lista de trayectos del conductor

--- trunk/taxiFree/src/domain/Conductor.java
+++ trunk/taxiFree/src/domain/Conductor.java
@@ -1,22 +1,38 @@
 package domain;
 
-import java.util.Collection;
+import java.util.List;
 
 
 public class Conductor extends Usuario  {
+	
+	private List<Trayecto> trayectos;
 
 	/** 
 	 * @uml.property name="vehiculo"
 	 * @uml.associationEnd multiplicity="(1 -1)" inverse="conductor:modelado.Vehiculo"
 	 */
-	private Collection <Vehiculo> vehiculo;
+	private List <Vehiculo> vehiculo;
+	
+	public List<Trayecto> getTrayectos() {
+		return trayectos;
+	}
+
+	public void setTrayectos(List<Trayecto> trayectos) {
+		this.trayectos = trayectos;
+	}
+
+	/** 
+	 * @uml.property name="viaje"
+	 * @uml.associationEnd multiplicity="(1 -1)" inverse="conductor:modelado.Viaje"
+	 */
+	private List<Viaje> viaje;
 
 	/** 
 	 * Getter of the property <tt>vehiculo</tt>
 	 * @return  Returns the vehiculo.
 	 * @uml.property  name="vehiculo"
 	 */
-	public Collection<Vehiculo> getVehiculo() {
+	public List<Vehiculo> getVehiculo() {
 		return vehiculo;
 	}
 
@@ -25,22 +41,18 @@ public class Conductor extends Usuario  {
 	 * @param vehiculo  The vehiculo to set.
 	 * @uml.property  name="vehiculo"
 	 */
-	public void setVehiculo(Collection<Vehiculo> vehiculo) {
+	public void setVehiculo(List<Vehiculo> vehiculo) {
 		this.vehiculo = vehiculo;
 	}
 
-	/** 
-	 * @uml.property name="viaje"
-	 * @uml.associationEnd multiplicity="(1 -1)" inverse="conductor:modelado.Viaje"
-	 */
-	private Collection<Viaje> viaje;
+
 
 	/** 
 	 * Getter of the property <tt>viaje</tt>
 	 * @return  Returns the viaje.
 	 * @uml.property  name="viaje"
 	 */
-	public Collection<Viaje> getViaje() {
+	public List<Viaje> getViaje() {
 		return viaje;
 	}
 
@@ -49,7 +61,7 @@ public class Conductor extends Usuario  {
 	 * @param viaje  The viaje to set.
 	 * @uml.property  name="viaje"
 	 */
-	public void setViaje(Collection<Viaje> viaje) {
+	public void setViaje(List<Viaje> viaje) {
 		this.viaje = viaje;
 	}
 
