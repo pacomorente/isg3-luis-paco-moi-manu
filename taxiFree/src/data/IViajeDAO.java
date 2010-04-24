ea747f1c6ec8b97dca39fc3e0e9b31c717bac836
pacomorente <pacomorente@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-24 23:53:11 +0000

    Se añade el método delete en JDBCViaje para complementar al realizado en JDBCTrayecto

--- taxiFree/src/data/IViajeDAO.java
+++ taxiFree/src/data/IViajeDAO.java
@@ -2,12 +2,14 @@ package data;
 
 import java.util.List;
 
+import java.sql.Connection;
+
 import domain.Viaje;
 
 public interface IViajeDAO {
 	public void insertViaje(Viaje v, String userOID);
 	public List<Viaje> selectAllViajes();
 	public Viaje selectViaje(String s);
-	public void deleteViaje(String numViaje);
+	public void deleteViaje(Connection conn, String ViajeOID);
 	public void updateViaje(Viaje v, String userOID);
 }
