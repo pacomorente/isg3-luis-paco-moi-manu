7dc81d07f846b548868dbb2b3e1918b3d2eb414a
pacomorente <pacomorente@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-24 23:55:05 +0000

    Se crea DAO de Trayecto ( origen y destino del conductor) en relación con Viaje

---
+++ trunk/taxiFree/src/data/JDBCTrayectoDAO.java
@@ -0,0 +1,62 @@
+package data;
+
+import java.util.List;
+import java.sql.Connection;
+
+import domain.Trayecto;
+
+public class JDBCTrayectoDAO implements ITrayectoDAO{
+
+	IViajeDAO vdao;
+    
+	public JDBCTrayectoDAO() {
+		vdao = new JDBCViajeDAO();
+    }
+	
+	@Override
+	public void deleteTrayectoOID(Connection conn, String viajeOID) {
+		// TODO Auto-generated method stub
+		/* comprobar si existen viajes con pasajeros, o bien se podría
+		 * poner el viaje como dado de baja 
+		 * AÑADIR CAMPO EN LA TABLA VIAJE: Baja:boolean
+		 */
+		//if ( ! vdao.existePasajeroViaje(conn, t.getOrigenTrayecto(),t.getDestinoTrayecto(), viajeOID)){
+			vdao.deleteViaje(conn,viajeOID);
+		//}else{
+			//lanzar mensaje de no borrar o PONER VIAJE A "BAJA"
+		//}
+		
+		
+		
+	}
+
+	@Override
+	public void insertTrayectoOID(Trayecto trayecto, String userOID) {
+		// TODO Auto-generated method stub
+		/*
+		 * añadir trayecto en la tabla VIAJE
+		 */
+		
+	}
+
+	@Override
+	public List<Trayecto> selectAllTrayectos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Trayecto selectTrayectoOID(Trayecto trayecto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateTrayectoOID(Trayecto trayecto, String userOID) {
+		// TODO Auto-generated method stub
+		/*
+		 * añadir trayecto en la tabla VIAJE
+		 */
+	}
+
+}
