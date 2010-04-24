832d28747332d840dbf347e7bc5de38044cc7365
pacomorente <pacomorente@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-24 23:56:26 +0000

    Se define los datos necesarios para el rol de conductor cuando genere sus trayectos.

--- taxiFree/src/domain/Trayecto.java
+++ taxiFree/src/domain/Trayecto.java
@@ -1,6 +1,36 @@
 package domain;
 
+import java.util.Date;
+
 
 public class Trayecto {
+	
+	private String origenTrayecto;
+	private String destinoTrayecto;
+	private Date fechaSalida;
+	
+	public String getOrigenTrayecto() {
+		return origenTrayecto;
+	}
+
+	public void setOrigenTrayecto(String origenTrayecto) {
+		this.origenTrayecto = origenTrayecto;
+	}
+
+	public String getDestinoTrayecto() {
+		return destinoTrayecto;
+	}
+
+	public void setDestinoTrayecto(String destinoTrayecto) {
+		this.destinoTrayecto = destinoTrayecto;
+	}
+
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
 
 }
