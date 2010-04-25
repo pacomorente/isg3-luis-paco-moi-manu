ecb7210dfe45680b2f95f6e6f74cbae44dd2d102
isg209.x11.moi@gmail.com <isg209.x11.moi@gmail.com@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-04-25 18:10:05 +0000

    Implementacion del DAO de Pasajero.

--- trunk/taxiFree/src/data/JDBCPasajeroDAO.java
+++ trunk/taxiFree/src/data/JDBCPasajeroDAO.java
@@ -1,21 +1,118 @@
 package data;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 import domain.Pasajero;
+import domain.Usuario;
 
 public class JDBCPasajeroDAO implements IPasajeroDAO {
 
-	@Override
-	public Pasajero selectPasajero(String nick) {
-		// TODO Auto-generated method stub
-		return null;
+	private Connection con;
+	private IUsuarioDAO udao;
+	
+	public JDBCPasajeroDAO(){
+		con = ConnectionManager.getInstance().checkOut();
+		udao = new JDBCUsuarioDAO();
+	}
+	
+	
+	public Pasajero selectPasajero(Connection conn, String pasajeroOID) {
+		PreparedStatement stmt = null;
+        ResultSet result = null;
+        Pasajero p = null;
+        String sql = "SELECT * FROM pasajero p WHERE (OID = ?) ";
+
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, pasajeroOID);
+            result = stmt.executeQuery();
+
+            result.next();
+            
+            p = new Pasajero();
+			//String oidPasajero = result.getString("OIDPasajero");
+            String oidUsuario = result.getString("OIDUsuario");
+			//String oidViaje = result.getString("OIDViaje");
+            
+            Usuario u = udao.select(con, oidUsuario);
+            p.setNombre(u.getNombre());	
+            p.setApellidos(u.getApellidos());
+            p.setCorreo(u.getCorreo());	
+            p.setDni(u.getDni());	
+            p.setNick(u.getNick());	
+            p.setApellidos(u.getPass());	
+            p.setEstrella(u.getEstrella());
+            
+        } catch (SQLException e) {
+            System.out.println("Message: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("ErrorCode: " + e.getErrorCode());
+        } finally {
+            try {
+                if (result != null) {
+                    result.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
+        return p;
 	}
 
 	@Override
 	public List<Pasajero> selectAllPasajeros() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement stmt = null;
+		List<Pasajero> searchResults = new LinkedList<Pasajero>();
+		ResultSet result = null;
+		
+		String sql = "SELECT * FROM pasajero ";
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.executeQuery();
+			result = stmt.executeQuery();
+			while (result.next()) {
+				Pasajero p = new Pasajero();
+				//String oidPasajero = result.getString("OIDPasajero");
+	            String oidUsuario = result.getString("OIDUsuario");
+				//String oidViaje = result.getString("OIDViaje");
+	            
+	            Usuario u = udao.select(con, oidUsuario);
+	            p.setNombre(u.getNombre());	
+	            p.setApellidos(u.getApellidos());
+	            p.setCorreo(u.getCorreo());	
+	            p.setDni(u.getDni());	
+	            p.setNick(u.getNick());	
+	            p.setApellidos(u.getPass());	
+	            p.setEstrella(u.getEstrella());
+	            
+	            searchResults.add(p);
+			}
+			result.close();
+			stmt.close();
+		} catch (SQLException e) {
+			System.out.println("Message: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("ErrorCode: " + e.getErrorCode());
+		}finally{
+			ConnectionManager.getInstance().checkIn(con);
+            try {
+                if (result != null)
+                    result.close();
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+            }
+		}
+		return searchResults;
 	}
 
 }
