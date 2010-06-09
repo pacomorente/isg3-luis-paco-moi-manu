44ebc7c7699019f0bff2e052e17864674b74b640
pacomorente <pacomorente@a67b1394-27a6-11df-9191-49dca8b91aeb>
2010-06-09 18:19:55 +0000

    se elimina la conexion conn que había global, ya que la que se usa en viaje viene dada desde conductor

--- taxifree/src/data/IViajeDAO.java
+++ taxifree/src/data/IViajeDAO.java
@@ -14,17 +14,24 @@ import domain.Viaje;
  */
 public interface IViajeDAO {
 	public void insertViaje(Connection conn, String OIDViaje, Viaje v );
+	
 	public List<Viaje> selectAllViajes();
 	public Viaje selectViaje(Connection conn, String s);
 	public String selectViajeOID(Connection con,String idViaje);
 	public List<Viaje> selectViajes(Connection con, String oidPasajero);
-	public void deleteViaje(String ViajeOID);
-	//public void modificaViaje(Connection con, String userOID);
+	
+	public void deleteViaje(Connection con,String ViajeOID);
 	public Viaje selectViajeConductor(Connection con,String oidViajeConductor);
+	//public void deleteViaje(String ViajeOID);
+	//public void modificaViaje(Connection con, String userOID);
+	
 	//public List<String> obtenerViajesOIDConductor(Connection conn, String oidc);
+	
 	public Viaje  cambiaEstadoViaje(Connection con, String idViaje);
 	public boolean  eliminaViaje(Connection con, String idViaje);
+	
 	public void updateViaje(Connection conn, Viaje viaje);
+	
 	public boolean existePasajerosViaje(Connection con, String idViaje);
 	public boolean existeViaje(Connection con,Viaje viaje);
 }
